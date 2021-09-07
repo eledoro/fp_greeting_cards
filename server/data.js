7431e2e66aa9ddf1ba3ca4720009fb44d94b0d02
eledoro <lena@booggee.com>
2021-09-07 16:55:16 +0100

    Edited eventType to lowcase

--- server/data.js
+++ server/data.js
@@ -1,9 +1,9 @@
 // My cats resource
 
 const cards = [
-    { id: 1, eventType: 'Birthday', greeting : 'Hi there!', 
+    { id: 1, eventType: 'birthday', greeting : 'Hi there!', 
     message: 'Have a great day!', background:'' },
-    { id: 2, eventType: 'Wedding', greeting : 'Hi Tom and Mary!', 
+    { id: 2, eventType: 'wedding', greeting : 'Hi Tom and Mary!', 
     message: 'Congrats!!', background:'' },
     
 ];
