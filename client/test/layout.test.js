55354b0638540a00bfa9be7f313ca9145fcdbb56
eledoro <lena@booggee.com>
2021-09-07 10:34:19 +0100

    Added  .html layout test

--- client/test/layout.test.js
+++ client/test/layout.test.js
@@ -19,6 +19,8 @@
         messageInput = document.querySelector('#message');
         submitBtn = document.querySelector('#submit');
         resetBtn = document.querySelector('#reset');
+        seeSavedBtn = document.querySelector('#seeSaved');
+        saveCurrentCardBtn=document.querySelector('#saveCurrentCard')
 
     })
     // test mainPage layout exists
@@ -45,5 +47,46 @@
       test('reset button exists', ()=>{
         expect(resetBtn).toBeTruthy()
       })
+      test(' see saved button exists', ()=>{
+        expect(seeSavedBtn).toBeTruthy()
+      })
+      test(' save current card button exists', ()=>{
+        expect(saveCurrentCardBtn).toBeTruthy()
+      })
+
+      
+ //   ------------  placeholders
+    test('it has a placeholder of "Greeting"', () => {
+      expect(formGreeting.getAttribute('placeholder')).toBe('Type your greeting')
+    
+  })
+      test('it has a placeholder of "Message"', () => {
+      expect(formMessage.getAttribute('placeholder')).toContain('Type your message')
+      })
+  
+      
+  
+  
+ 
+  
+      // ---------   has labels  
+      test('Greeting has a label', () => {
+        let label = document.querySelector('label[for="greeting"]')
+      
+      expect(label.textContent).toContain("Add your greeting")
+      })
+      test('Event has a label', () => {
+        let label = document.querySelector('label[for="event"]')
+      
+      expect(label.textContent).toContain("Choose an event")
+      })
+      test('Message has a label', () => {
+        let label = document.querySelector('label[for="message"]')
+      
+      expect(label.textContent).toContain("Add your message")
+      })
+
+
+
 
  })
